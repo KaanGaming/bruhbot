12d2221a07e0f74525cbf2d7925b5f38a6eb5b7c
KaanGaming <kaangaming.gamejolt@gmail.com>
2019-10-23 21:50:31 +0300

    I am sorry 1/2

---
+++ commands/loganpaul.js
@@ -0,0 +1,39 @@
+module.exports = {
+    execute(message, client, args) {
+		if (message.author.id != "306455766586359809") return;
+        message.channel.send('Yah, yeet');
+		setTimeout({
+			message.channel.send("Little brother Jakey try to roast me?");
+			setTimeout({
+				message.channel.send("*What?*");
+			}, 100);
+		}, 1000);
+		setTimeout({
+			message.channel.send("Little brother standing on his own two feet?");
+			setTimeout({
+				message.channel.send("*Not for long*");
+			}, 100);
+		}, 2000);
+		setTimeout({
+			message.channel.send("I'm a dog, you're a puppy, call you Kong");
+		}, 3000);
+		setTimeout({
+			message.channel.send("Let's talk about your garbage that you call a song");
+		}, 4000);
+		setTimeout({
+			message.channel.send("It should be every other day, just some strong advice");
+		}, 5000);
+		setTimeout({
+			message.channel.send("I made you famous once, it's about to happen twice");
+		}, 6000);
+		setTimeout({
+			message.channel.send("Yeah, you on Disney but who helped you read all your lines");
+			setTimeout({
+				message.channel.send("*That's me*");
+			}, 100);
+		}, 7000);
+		setTimeout({
+			message.channel.send("Don't forget boy, you were my shadow on Vine");
+		}, 8000);
+	},
+};
